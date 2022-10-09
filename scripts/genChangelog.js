85c71114a5908058f7c6452aafda7a566e4d5335
shuisheng <2274751790@qq.com>
2022-10-09 15:42:26 +0800

    ci(custom): 修改脚本

--- scripts/genChangelog.js
+++ scripts/genChangelog.js
@@ -5,12 +5,14 @@ const { execSync, spawnSync } = require('child_process');
 async function genNewRelease() {
   const nextVersion = require('../lerna.json').version;
   // const res = execSync(`lerna-changelog --next-version ${nextVersion}`, {
-  //   // stdio: 'inherit',
+  //   // stdio: 'inherit',//不能设置该属性，不然输出就一直都是null
   // });
   const res = spawnSync(`lerna-changelog`, ['--next-version', nextVersion], {
-    stdio: 'inherit',
+    // stdio: 'inherit', //不能设置该属性，不然输出就一直都是null
   });
   if (res.status) {
+    console.log('lerna-changelog错误！');
+    console.log(res.error);
     process.exit(res.status);
   }
   return res.stdout;
